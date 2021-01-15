a6bc456d829530d3cb5b91eacec5b5fd356ba90c
Shane Upchurch <shaneupchurch@gmail.com>
2021-01-15 15:09:30 -0600

    external link model working and tests passing, still need to add auth

--- __tests__/links.test.js
+++ __tests__/links.test.js
@@ -6,7 +6,9 @@ const app = require('../lib/app');
 describe('Link endpoint', () => {
   beforeEach(() => {
     return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
-
+  });
+  beforeEach(() => {
+    return pool.query(fs.readFileSync('./sql/test.sql', 'utf-8'));
   });
   afterAll(() => {
     return pool.end();
@@ -14,14 +16,40 @@ describe('Link endpoint', () => {
 
   it('creates a new link via POST', async() => {
     const res = await request(app)
-      .post('/links')
+      .post('/api/v1/links')
       .send({
         url: 'http://www.youtube.com'
       });
-
     expect(res.body).toEqual({
-      id: '1',
+      id: `${res.body.id}`,
       url: 'http://www.youtube.com'
     });
+  });
+
+  it('finds a link by id via GET', async() => {
+    const res = await request(app)
+      .get('/api/v1/links/2');
+    expect(res.body).toEqual({
+      id: '2',
+      url: 'https://www.youtube.com/watch?v=Re-h_rtttIE'
+    });
+  });
+
+  it('returns all links via GET', async() => {
+    const res = await request(app)
+      .get('/api/v1/links');
+    expect(res.body).toHaveLength(3);
+    expect(res.body).toEqual([
+      { 'id':'1', 'url':'https://www.youtube.com/watch?v=wnlcuZ0mJSU' }, 
+      { 'id':'2', 'url':'https://www.youtube.com/watch?v=Re-h_rtttIE' }, 
+      { 'id':'3', 'url':'https://www.youtube.com/watch?v=SuScgzVJp-s' }]);
+  });
+
+  it('delets a link via DELETE', async() => {
+    const res = await request(app)
+      .delete('/api/v1/links/2');
+    expect(res.body).toEqual(
+      { 'id':'2', 'url':'https://www.youtube.com/watch?v=Re-h_rtttIE' }
+    );
   });
 });
