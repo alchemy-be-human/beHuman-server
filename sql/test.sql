82ab41d414f718a3d9768364e4c13e314568cb9e
Shane Upchurch <shaneupchurch@gmail.com>
2021-01-15 15:24:35 -0600

    Feature/5 data model external link (#1)
    * files setup for external link model
    
    * external link model working and tests passing, still need to add auth

---
+++ sql/test.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO links (url) VALUES ('https://www.youtube.com/watch?v=wnlcuZ0mJSU');
+INSERT INTO links (url) VALUES ('https://www.youtube.com/watch?v=Re-h_rtttIE');
+INSERT INTO links (url) VALUES ('https://www.youtube.com/watch?v=SuScgzVJp-s');
