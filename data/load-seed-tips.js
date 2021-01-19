c81cb0b75f9792429e84f646f1ea23d63f36cc54
Bryana Kitchen <67290473+bryanakitchen@users.noreply.github.com>
2021-01-19 16:30:58 -0500

    Feature/20 seed quick tip data (#4)
    * add tips
    
    * add load seed tips function
    
    * add create tables and drop tables
    
    * working seed data

---
+++ data/load-seed-tips.js
@@ -0,0 +1,26 @@
+const pool = require('../lib/utils/pool');
+const tipsData = require('./tips-data');
+
+run();
+
+async function run() {
+
+  try {
+    await pool.connect();
+
+    await Promise.all(
+      tipsData.map(tip => {
+        return pool.query(`
+                    INSERT INTO tips (tip)
+                    VALUES ($1)`,
+        [tip.tip]);
+      })
+    );
+  } 
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    pool.end();
+  }
+}
