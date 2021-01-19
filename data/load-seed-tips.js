88748f1eca4d0f26ddc731d0397336a88842f8bf
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-01-19 14:30:21 -0500

    add load seed tips function

--- data/load-seed-tips.js
+++ data/load-seed-tips.js
@@ -1,1 +1,26 @@
 const pool = require('../lib/utils/pool');
+const tips = require('./tips');
+
+run();
+
+async function run() {
+
+  try {
+    await pool.connect();
+
+    await Promise.all(
+      tips.map(tip => {
+        return pool.query(`
+                    INSERT INTO tips (tip)
+                    VALUES ($1)`,
+        [tip.tip]);
+      })
+    );
+  } 
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    pool.end();
+  }
+}
