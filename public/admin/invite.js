3229017212ce0d039da79c197b2de2d87b0c6391
Jillian Gibson <jillian.l.gibson@gmail.com>
2021-01-21 17:29:10 -0800

    admin files clean

--- public/admin/invite.js
+++ public/admin/invite.js
@@ -1,24 +1,17 @@
 const request = superagent;
 
-// INVITE SECTION
 const inviteForm = document.querySelector('#invite-form');
 
 inviteForm.addEventListener('submit', async(e) => {
   e.preventDefault();
-  console.log('Invite form was clicked.');
 
   const data = new FormData(inviteForm);
 
   const invite = {
     email: data.get('email-input'),
   };
-  console.log(invite);
-
-  //send form data to backend below here with POST/fetch call
 
   await request
     .post('/api/v1/auth/invite')
-    .send(invite)
-    .then(console.log('Made it here.'));
-
+    .send(invite);
 });
